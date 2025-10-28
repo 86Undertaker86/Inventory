806e258c1c8d005442a85c2cf911df4afe6af447
undertaker <d.sidorchuk07@gmail.com>
2025-10-28 13:44:53 +0200

    Edit url for manager and operator in InventoryController

--- src/main/java/com/CourseWork/Inventory/Controller/InventoryController.java
+++ src/main/java/com/CourseWork/Inventory/Controller/InventoryController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/inventory")
 public class InventoryController {
 
     private final InventoryService inventoryService;
@@ -17,10 +16,16 @@ public class InventoryController {
         this.inventoryService = inventoryService;
     }
 
-    // 🧾 Перегляд залишків
-    @GetMapping
-    public String showInventoryPage(Model model) {
+    @GetMapping("/manager/inventory")
+    public String showManagerInventory(Model model) {
+        model.addAttribute("inventories", inventoryService.getAllInventories());
+        return "InventoryPage";
+    }
+
+    @GetMapping("/operator/inventory")
+    public String showOperatorInventory(Model model) {
         model.addAttribute("inventories", inventoryService.getAllInventories());
         return "InventoryPage";
     }
 }
+
