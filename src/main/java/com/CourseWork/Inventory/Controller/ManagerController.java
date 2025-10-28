48a4cca537f383a21641a798bd7238cba65f5673
undertaker <d.sidorchuk07@gmail.com>
2025-10-28 12:52:42 +0200

    Optimize ManagerController. Everything else has been removed.

--- src/main/java/com/CourseWork/Inventory/Controller/ManagerController.java
+++ src/main/java/com/CourseWork/Inventory/Controller/ManagerController.java
@@ -1,34 +1,15 @@
 package com.CourseWork.Inventory.Controller;
 
-import com.CourseWork.Inventory.Model.*;
-import com.CourseWork.Inventory.Repository.*;
-import com.CourseWork.Inventory.Service.StockMovementService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/manager")
 public class ManagerController {
 
-    private final StockMovementService stockService;
-
-    public ManagerController(StockMovementService stockService) {
-        this.stockService = stockService;
-    }
-
     // Головна сторінка менеджера
     @GetMapping
-    public String showMovementsForManager(Model model) {
-        model.addAttribute("role", "MANAGER");
-        model.addAttribute("movements", stockService.getAllMovements());
+    public String showMovementsForManager() {
         return "ManagerPage";
-    }
-
-    // Реєстрація нового руху
-    @PostMapping("/add")
-    public String addMovement(@ModelAttribute("movement") StockMovement movement) {
-        stockService.saveMovement(movement);
-        return "redirect:/manager?success";
     }
 }
